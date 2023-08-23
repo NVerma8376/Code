86d00e66f1c4142ab3c03f4e152e9ad3b5874c0b
NVerma8376 <pythons123456789@gmail.com>
2023-08-23 22:36:26 +0530

    testing some stuff

---
+++ Python/Big_projects/PI_calc_2.py
@@ -0,0 +1,12 @@
+from random import *
+import math
+
+height = 100
+width = 100
+square_area = 100*100
+
+radius = 50
+circle_area = math.pi*radius**2
+
+x = randint(0, height)
+y = randint(0, width)
